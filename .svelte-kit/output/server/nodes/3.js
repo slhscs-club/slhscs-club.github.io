583fb286d2161e3ae03cb0d598dda78850237bc5
packjackisback <k1401308@students.katyisd.org>
2026-07-27 22:40:39 -0500

    fixing ics

--- .svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/about/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.B4ww9V27.js","_app/immutable/chunks/C9Lu5ERF.js","_app/immutable/chunks/CnRo4IZZ.js","_app/immutable/chunks/DO_C8pz5.js","_app/immutable/chunks/Bh8bEwaV.js","_app/immutable/chunks/D-Xnmb3p.js","_app/immutable/chunks/_B4zu5cQ.js","_app/immutable/chunks/BKWqbsF9.js","_app/immutable/chunks/CyqSIwg9.js"];
-export const stylesheets = ["_app/immutable/assets/3.CRJA5xpG.css"];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/7.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 7;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/join/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/7.BFyvvAvO.js","_app/immutable/chunks/C9Lu5ERF.js","_app/immutable/chunks/CnRo4IZZ.js","_app/immutable/chunks/_B4zu5cQ.js","_app/immutable/chunks/DO_C8pz5.js","_app/immutable/chunks/D-Xnmb3p.js","_app/immutable/chunks/BKWqbsF9.js"];
-export const stylesheets = ["_app/immutable/assets/7.CJIyRcyB.css"];
-export const fonts = [];
